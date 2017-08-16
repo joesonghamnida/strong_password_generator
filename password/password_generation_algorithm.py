2a7c5758eb8fa1f107cb3ef792d3b7b9388a7da8
Joe Morris <joe.morris.dev@gmail.com>
2017-08-15 22:15:54 -0500

    works but will need to make truly random

---
+++ password/password_generation_algorithm.py
@@ -0,0 +1,34 @@
+import random
+
+characters = 'abcdefghijklmnopqrstuvwxyz'
+
+numbers = '1234567890'
+
+symbols = '!@#$%^&*()_+-='
+
+
+def is_letter_uppercase(letter):
+    number = random.randint(0, 1)
+    if number == 0:
+        letter = letter.upper()
+    return letter
+
+
+def generate_password():
+    new_password = ""
+
+    i = 0
+    while i < 24:
+        character_set_selector = random.randrange(0, 2)
+
+        if character_set_selector == 0:
+            letter = characters[random.randint(0, 25)]
+            letter = is_letter_uppercase(letter)
+            new_password = new_password + letter
+        elif character_set_selector == 1:
+            new_password = new_password + numbers[random.randint(0, 9)]
+        else:
+            new_password + new_password + symbols[random.randint(0, 13)]
+        i = i + 1
+
+    return new_password
