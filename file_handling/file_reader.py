e0f74f688e501b4cf7df97c4261c0724e2c84915
Joe Morris <joe.morris.dev@gmail.com>
2017-09-05 02:52:09 -0500

    will need to refactor

--- file_handling/file_reader.py
+++ file_handling/file_reader.py
@@ -1,1 +1,13 @@
-import os
+def read_from_file():
+
+    passwords = []
+    path = '/Users/joe/programming/python/new/password_generator/prev_passwords.txt'
+
+    file = open(path, 'r')
+    #TODO: will need to decrypt and check file contents
+    i = 0
+    while file.readline():
+        passwords.append(file.readline())
+    file.close()
+
+    return passwords
--- file_handling/file_writer.py
+++ file_handling/file_writer.py
@@ -1,1 +1,7 @@
-import os
+def write_to_file(password):
+    path = '/Users/joe/programming/python/new/password_generator/prev_passwords.txt'
+
+    file = open(path, 'a')
+    #TODO: will need to encrypt file contents
+    file.write(password + '\n')
+    file.close()
