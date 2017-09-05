bdb197b04aa7a0c2f9902c1805b064e7268c0d49
Joe Morris <joe.morris.dev@gmail.com>
2017-09-05 02:51:44 -0500

    will need to refactor

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import password.password_generation_algorithm
+import password.prev_password_checker
+from file_handling import file_writer
+from file_handling import file_reader
 
 print("Random strong password generator.")
 
-print(password.password_generation_algorithm.generate_password())
+generated_password = password.password_generation_algorithm.generate_password()
+
+print(generated_password)
+
+file_writer.write_to_file(generated_password)
+file_reader.read_from_file()
+
+password.prev_password_checker.do_passwords_match(generated_password)
